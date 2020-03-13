1ffce9e2c99efe6f915046b0c8ddb1357e2797d2
gyh777 <1486546336@qq.com>
2020-03-13 20:51:39 +0800

    mvc.xml

--- hr/src/com/hr/mapper/EngageSubjectsMapper.java
+++ hr/src/com/hr/mapper/EngageSubjectsMapper.java
@@ -3,7 +3,9 @@ package com.hr.mapper;
 import java.util.List;
 
 import com.hr.pojo.Bonus;
+import com.hr.pojo.EngageSubjects;
 
 public interface EngageSubjectsMapper {
+	public int insert(EngageSubjects engageSubjects);
 	
 }
--- hr/src/com/hr/pojo/EngageSubjects.java
+++ hr/src/com/hr/pojo/EngageSubjects.java
@@ -21,6 +21,13 @@ public class EngageSubjects {
 
     private String correctKey;
 
+    private String content;
+	private String keyA;
+	private String keyB;
+	private String keyC;
+	private String keyD;
+	private String keyE;
+    
     private String changer;
 
     private Date changeTime;
@@ -112,4 +119,53 @@ public class EngageSubjects {
     public void setChangeTime(Date changeTime) {
         this.changeTime = changeTime;
     }
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getKeyA() {
+		return keyA;
+	}
+
+	public void setKeyA(String keyA) {
+		this.keyA = keyA;
+	}
+
+	public String getKeyB() {
+		return keyB;
+	}
+
+	public void setKeyB(String keyB) {
+		this.keyB = keyB;
+	}
+
+	public String getKeyC() {
+		return keyC;
+	}
+
+	public void setKeyC(String keyC) {
+		this.keyC = keyC;
+	}
+
+	public String getKeyD() {
+		return keyD;
+	}
+
+	public void setKeyD(String keyD) {
+		this.keyD = keyD;
+	}
+
+	public String getKeyE() {
+		return keyE;
+	}
+
+	public void setKeyE(String keyE) {
+		this.keyE = keyE;
+	}
+    
 }
