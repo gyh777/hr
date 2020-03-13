ff21b581d1845977fb8327bc79bfeb42ddc29864
gyh777 <1486546336@qq.com>
2020-03-13 15:50:12 +0800

    Mapper

---
+++ hr/src/com/hr/mapper/HumanFileDigMapper.java
@@ -0,0 +1,13 @@
+package com.hr.mapper;
+
+import java.util.List;
+
+import com.hr.pojo.Bonus;
+
+public interface HumanFileDigMapper {
+	public boolean saveBonus(Bonus bonus);
+	public List<Bonus> selectAllBonus();
+	public Bonus selectBonusByBon_id(int bon_id);
+	public boolean deleteStudentByBon_id(int bon_id);
+	public boolean updateBonus(Bonus bonus);
+}
