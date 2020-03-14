0e76d209a3efd143477e8c88ebeb576c2235e789
chenyu473 <282237742@qq.com>
2020-03-14 14:44:58 +0800

    档案的mapper和service

--- hr/src/com/hr/mapper/HumanFileMapper.java
+++ hr/src/com/hr/mapper/HumanFileMapper.java
@@ -2,8 +2,12 @@ package com.hr.mapper;
 
 import java.util.List;
 
-import com.hr.pojo.Bonus;
+import com.hr.pojo.HumanFile;
 
 public interface HumanFileMapper {
-	
+	public boolean addHumanFile(HumanFile huf);
+    public boolean removeHumanFile(int hufId);
+    public boolean updateHumanFile(HumanFile huf);
+    public HumanFile queryHumanFileById(int hufId);
+    public List<HumanFile> queryAllHumanFile();
 }
