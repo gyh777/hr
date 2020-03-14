60566e16d3a89f1acf5b42ccabce9cc406dee6dd
nuenue666 <nuenue666@neunue666>
2020-03-14 14:37:02 +0800

    添加了Major对应的Controller

---
+++ hr/src/com/hr/web/controller/ConfigMajorController.java
@@ -0,0 +1,22 @@
+package com.hr.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hr.service.ConfigMajorService;
+
+@Controller
+@RequestMapping("/configMajor")
+public class ConfigMajorController {
+	@Autowired
+	ConfigMajorService configMajorServiceImpl;
+	
+	@RequestMapping("/toDelete")
+	public String toDelete(int makId) {
+		System.out.println("toDelete");
+//		configMajorServiceImpl.deleteConfigMajor(makId);
+		return null;
+	}
+	
+}
