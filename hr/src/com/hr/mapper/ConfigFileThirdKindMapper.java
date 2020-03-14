b9dea8367552970abfc263a3ad78a17a6d034173
chenyu473 <282237742@qq.com>
2020-03-14 10:53:53 +0800

    cy-------写了二级三级机构mapper和service，你们的报错了就不能测试

--- hr/src/com/hr/mapper/ConfigFileThirdKindMapper.java
+++ hr/src/com/hr/mapper/ConfigFileThirdKindMapper.java
@@ -1,5 +1,13 @@
 package com.hr.mapper;
 
-public interface ConfigFileThirdKindMapper {
+import java.util.List;
 
+import com.hr.pojo.ConfigFileThirdKind;
+
+public interface ConfigFileThirdKindMapper {
+	public boolean addConfigFileThirdKind(ConfigFileThirdKind ftk);
+    public boolean removeConfigFileThirdKind(int ftkId);
+    public boolean updateConfigFileThirdKind(ConfigFileThirdKind ftk);
+    public ConfigFileThirdKind queryConfigFileThirdKindById(int ftkId);
+    public List<ConfigFileThirdKind> queryAllConfigFileThirdKind();
 }
--- hr/src/com/hr/test/Test.java
+++ hr/src/com/hr/test/Test.java
@@ -6,16 +6,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hr.pojo.Bonus;
-import com.hr.pojo.ConfigFileFirstKind;
+import com.hr.pojo.ConfigFileSecondKind;
 import com.hr.service.BonusService;
-import com.hr.service.ConfigFileFirstKindService;
+import com.hr.service.ConfigFileSecondKindService;
 
 public class Test {
   public static void main(String[] args) {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		ConfigFileFirstKindService service = (ConfigFileFirstKindService)context.getBean("configFileFirstKindServiceImpl");
-		List<ConfigFileFirstKind> list = service.queryAllConfigFileFirstKind();
+//		ConfigFileSecondKindService service = (ConfigFileSecondKindService)context.getBean("configFileSecondKindServiceImpl");
+//		List<ConfigFileSecondKind> list = service.queryAllConfigFileSecondKind();
+//		System.out.println(list.size());
+		
+		ConfigFileSecondKindService service = (ConfigFileSecondKindService)context.getBean("configFileSecondKindServiceImpl");
+		List<ConfigFileSecondKind> list = service.queryAllConfigFileSecondKind();
 		System.out.println(list.size());
 }
 }
