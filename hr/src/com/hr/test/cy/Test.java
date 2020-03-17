ad00130c3cdba373303f36bcd5e6b9e8baebcb57
chenyu473 <282237742@qq.com>
2020-03-17 14:54:51 +0800

    cy----档案新建

--- hr/src/com/hr/test/cy/Test.java
+++ hr/src/com/hr/test/cy/Test.java
@@ -1,4 +1,4 @@
-package com.hr.test;
+package com.hr.test.cy;
 
 import java.util.List;
 
@@ -8,15 +8,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.hr.pojo.Bonus;
 import com.hr.pojo.ConfigFileFirstKind;
 import com.hr.pojo.ConfigFileSecondKind;
+import com.hr.pojo.HumanFile;
 import com.hr.service.BonusService;
 import com.hr.service.ConfigFileFirstKindService;
 import com.hr.service.ConfigFileSecondKindService;
+import com.hr.service.HumanFileService;
 
 public class Test {
   public static void main(String[] args) {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		ConfigFileSecondKindService service = (ConfigFileSecondKindService)context.getBean("configFileSecondKindServiceImpl");
+//		ConfigFileSecondKindService service = (ConfigFileSecondKindService)context.getBean("configFileSecondKindServiceImpl");
 //		List<ConfigFileSecondKind> list = service.queryAllConfigFileSecondKind();
 //		System.out.println(list.size());
 		
@@ -32,15 +34,29 @@ public class Test {
 //		ConfigFileFirstKind cffk =   service.queryConfigFileFirstKindById("03");
 //		service.removeConfigFileFirstKind("03");
 //		System.out.println(cffk);
+		
+		
+		
+		
 //		short s = 1;
-		ConfigFileSecondKind cffk =   new ConfigFileSecondKind();
+//		ConfigFileSecondKind cffk =   new ConfigFileSecondKind();
 //		cffk.setFsk_id(s);
-		cffk.setSecond_kind_id("02");
-		cffk.setSecond_kind_name("分部");
-		cffk.setFirst_kind_id("02");
-		cffk.setFirst_kind_name("集团");
-		cffk.setSecond_kind_salary_id("002");
-		cffk.setSecond_kind_sale_id("045");
-		service.addConfigFileSecondKind(cffk);
+//		cffk.setSecond_kind_id("02");
+//		cffk.setSecond_kind_name("分部");
+//		cffk.setFirst_kind_id("02");
+//		cffk.setFirst_kind_name("集团");
+//		cffk.setSecond_kind_salary_id("002");
+//		cffk.setSecond_kind_sale_id("045");
+//		service.addConfigFileSecondKind(cffk);
+		
+		
+		HumanFileService service = (HumanFileService)context.getBean("humanFileServiceImpl");
+		HumanFile hf = new HumanFile();
+		hf.setHuman_name("sss");
+		Short age = 20;
+		hf.setHuman_age(age);
+		service.addHumanFile(hf);
+//		List<HumanFile> list= service.queryAllHumanFile();
+//		System.out.println(list.size());
   }
 }
