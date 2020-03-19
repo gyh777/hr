4a64b9b847c10f05afe9040a468e661a52d56fc1
nuenue666 <nuenue666@neunue666>
2020-03-20 02:18:39 +0800

    添加了一些做好的但丢失的功能

---
+++ hr/src/com/hr/web/controller/requestparamtype/SalaryStandardIdAndName.java
@@ -0,0 +1,19 @@
+package com.hr.web.controller.requestparamtype;
+
+public class SalaryStandardIdAndName {
+	private String standardId;
+	private String standardName;
+	public String getStandardId() {
+		return standardId;
+	}
+	public void setStandardId(String standardId) {
+		this.standardId = standardId;
+	}
+	public String getStandardName() {
+		return standardName;
+	}
+	public void setStandardName(String standardName) {
+		this.standardName = standardName;
+	}
+	
+}
