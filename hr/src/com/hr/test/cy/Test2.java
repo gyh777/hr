99ba57bfd590818a4a3797d56168f24785160f22
chenyu473 <282237742@qq.com>
2020-03-20 11:17:10 +0800

    CC

---
+++ hr/src/com/hr/test/cy/Test2.java
@@ -0,0 +1,31 @@
+package com.hr.test.cy;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hr.pojo.HumanFileDig;
+import com.hr.service.HumanFileDigService;
+import com.hr.service.impl.HumanFileDigServiceImpl;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("firstKindName", "sss");
+		map.put("secondKindName", "sss");
+		map.put("thirdKindName", "sss");
+		map.put("humanMajorName", "sss");
+		map.put("humanMajorKindName","sss");
+		map.put("str_startTime","2000-09-02");
+		map.put("str_endTime","2010-02-06");
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		HumanFileDigService service = (HumanFileDigService)context.getBean("humanFileDigServiceImpl");
+         List<HumanFileDig> list =       service.queryByMapCondition(map);
+	   System.out.println(list);
+	}
+
+}
